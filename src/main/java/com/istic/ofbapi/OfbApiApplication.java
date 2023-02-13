9153b0fc5cb78d11132a8ef81f978a1472489a16
Fritzgy Lubin <flubin@MacBook-Pro-de-Fritzgy.local>
2023-02-13 12:37:21 +0100

    Initial commit

---
+++ src/main/java/com/istic/ofbapi/OfbApiApplication.java
@@ -0,0 +1,13 @@
+package com.istic.ofbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfbApiApplication.class, args);
+	}
+
+}
